f0f4ca534753012fab12e4b104d5f320270cab09
RobSinn <robert@localhost.(none)>
2014-07-03 20:47:15 +1000

    System info + publish output to Thredds

--- Operation.py
+++ Operation.py
@@ -24,42 +24,72 @@ from netCDF4 import Dataset
 import sys
 import numpy
 import zoo
-import urllib
-import correlate
+import requests
+import correlation
+import os.path
+
+def getFileNameFromUrl(url):
+        return url.rsplit('/',1)
+
+def getLocation(url):
+        return "Thredds/sample/" + getFileNameFromUrl(url)[1]
+
+def readFileExistsInThredds(name):
+        return os.path.isfile(name)
+
+def downloadFile(url):
+        filePath = getLocation(url)
+        r = requests.get(url)
+        f = open(filePath, 'wb')
+        for chunk in r.iter_content(chunk_size=512 * 1024):
+                if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+        f.close()
+        return
+
+def outputFileName(operation,url1,url2):
+        name = str(operation) + '_' + getFileNameFromUrl(url1)[1].strip('.nc')
+        name += '_' + getFileNameFromUrl(url2)[1].strip('.nc') + '.nc'
+        return name
 
 def Operation(conf,inputs,outputs):
         urls = inputs["urls"]["value"].split(',')
+        outputFile = "Thredds/sample/" + outputFileName(inputs["selection"]["value"],urls[0],urls[1])
         if len(urls) < 2:
                         conf["lenv"]["message"] = "There has to be atleast two datasets"
                         return zoo.SERVICE_FAILED
         try:
-                output = Dataset('data/sample.nc', 'w', format='NETCDF4')
+                if readFileExistsInThredds(outputFile):
+                        os.remove(outputFile)
+                output = Dataset(outputFile, 'w', format='NETCDF4')
         except:
-                conf["lenv"]["message"] = "Could not open '" + "sample.nc" + "' for writing."
+                conf["lenv"]["message"] = "Could not open '" + outputFile + "' for writing."
                 return zoo.SERVICE_FAILED
         try:
-                #dataset1D = Dataset(, 'r', format='NETCDF4')
-                urllib.urlretrieve(urls[0],"data/url1.nc")
-                dataset1D = Dataset("data/url1.nc", 'r', format='NETCDF4')
+                #dataset1D = Dataset(urls[0], 'r', format='NETCDF4')
+                if readFileExistsInThredds(getLocation(urls[0])) == 0:
+                        downloadFile(urls[0])
+                dataset1D = Dataset(getLocation(urls[0]), 'r', format='NETCDF4')
         except:
                 conf["lenv"]["message"] = "Could not open '" + urls[0] + "' for reading."
                 return zoo.SERVICE_FAILED
         try:
-                #dataset3D = Dataset(inputs["url2"]["value"], 'r', format='NETCDF4')
-                urllib.urlretrieve(urls[1],"data/url2.nc")
-                dataset3D = Dataset("data/url2.nc", 'r', format='NETCDF4')
+                #dataset3D = Dataset(urls[1], 'r', format='NETCDF4')
+                if readFileExistsInThredds(getLocation(urls[1])) == 0:
+                        downloadFile(urls[1])
+                dataset3D = Dataset(getLocation(urls[1]), 'r', format='NETCDF4')
         except:
                 conf["lenv"]["message"] = "Could not open '" + urls[1] + "' for reading."
                 return zoo.SERVICE_FAILED
 
-        if inputs["selection"]["value"] == "Coorelate":
-                result = correlate.correlation(dataset1D, dataset3D, output)
+        if inputs["selection"]["value"] == "correlate":
+                result = correlation.correlation(dataset1D, dataset3D, output)
 
         dataset1D.close()
         dataset3D.close()
         output.close()
-        outputs["Result"]["value"]="http://130.56.248.143/data/sample.nc"
+        outputs["Result"]["value"]="http://130.56.248.143/" + outputFile
 
         return zoo.SERVICE_SUCCEEDED
-                                                                                                                                              63,10-17      Bot
+                                                                                                                                              90,2-9        Bot
 
