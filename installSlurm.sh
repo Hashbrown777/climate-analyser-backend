a028e9b0e28466ad36b1e775075bc11405791110
RobSinn <robert@localhost.(none)>
2014-08-29 23:21:06 +1000

    Slurm install working

--- installSlurm.sh
+++ installSlurm.sh
@@ -4,27 +4,27 @@ exit 1
 fi
 RUNASUSER="sudo -u $SUDO_USER"
 
-cp slurm.conf /usr/local/etc/slurm.conf
+cp slurm.conf /etc/slurm/slurm.conf
 yum -y install munge
-chown ec2-user /etc/munge
-chown ec2-user /var/lib/munge
-chown ec2-user /var/log/munge
-chown ec2-user /var/run/munge
+chown root /etc/munge
+chown root /var/lib/munge
+chown root /var/log/munge
 mkdir /var/run/munge
-chown ec2-user /var/run/munge
+chown root /var/run/munge
 dd if=/dev/urandom bs=1 count=1024 >/etc/munge/munge.key
-chown ec2-user /etc/munge/munge.key
+chown root /etc/munge/munge.key
 chmod 700 /etc/munge/munge.key
 
-mkdir Slurm
-cd Slurm
+yum -y install readline-devel
+yum -y install perl-devel
+yum -y install munge-devel
+yum -y install pam-devel
+yum -y install perl-DBI
 curl http://www.schedmd.com/download/latest/slurm-14.03.7.tar.bz2 -o slurm-14.03.7.tar.bz2
-tar --bzip -x -f slurm*tar.bz2
-cd slurm-14.03.7
-./configure
-make
-make install
+yum -y groupinstall "Development tools"
+rpmbuild -ta slurm*.tar.bz2
+rpm -i /root/rpmbuild/RPMS/x86_64/slurm-*
 
 mkdir /slurm
-mkdir /slurm/slurmd
+chown ec2-user /slurm
 
